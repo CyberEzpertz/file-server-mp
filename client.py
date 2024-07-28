638a42ad84c3cb30aa380f90248828fc7f81cf3e
DlnLimited <dylanlmt11@gmail.com>
2024-07-28 20:20:19 +0800

    feat: create class for easier use of attributes

--- client.py
+++ client.py
@@ -2,68 +2,84 @@ import socket
 s = socket.socket()
 
 def parser(inp):
-    return inp.split(' ')
+        return inp.split(' ')
 
-# Fetches a file from the server using a file name
-def fetch_dir():
-    pass
+class userConnection:
+    def __init__(self):
+        self.userName = None
+        self.connected = False
+        
+        self.server_IP = None
+        self.portNumber = None
+        
 
-# Fetches a file from the server using a file name
-def fetch_file(filename):
-    pass
+    # Fetches a file from the server using a file name
+    def fetch_dir(self):
+        pass
 
-# Sends a file to the server using the current client alias
-def send_file(filename):
-    pass
+    # Fetches a file from the server using a file name
+    def fetch_file(filename):
+        pass
 
-# Disconnects from the current server
-def register_alias():
-    pass
+    # Sends a file to the server using the current client alias
+    def send_file(filename):
+        pass
 
-# Disconnects from the current server
-def server_disconnect():
-    s.close()
-    s = socket.socket()
+    # registers the User
+    def register_alias(self, user):
+        self.userName = user
 
-# Connects with the server
-def server_connect(host, port):
-    s.connect((host,port))
+    # Disconnects from the current server
+    def server_disconnect(self):
+        s.close()
+        s = socket.socket()
+        self.connected = False
+
+    # Connects with the server
+    def server_connect(self):
+        s.connect((self.server_IP, self.portNumber))
+        self.connected = True
 
 
-# Prints the commands and their functions
-def print_help():
-    print("""
-        Available commands:
-        /join <server_ip_add> <port> - creates a connection with the server
-        /leave - leaves the connection with the server
-        /register <handle> - registers a user
-        /store <filename> - send a file to the server
-        /dir - requests a list of file names stored with the server
-        /get <filename> - requests the specific file from the directory of the server
-        /? - gets all input syntax commands shown above for reference
-        """)
+    # Prints the commands and their functions
+    def print_help():
+        print("""
+            Available commands:
+            /join <server_ip_add> <port> - creates a connection with the server
+            /leave - leaves the connection with the server
+            /register <handle> - registers a user
+            /store <filename> - send a file to the server
+            /dir - requests a list of file names stored with the server
+            /get <filename> - requests the specific file from the directory of the server
+            /? - gets all input syntax commands shown above for reference
+            """)
 
 
 # Ask for input while client is open
 while True:
     inp = input("> ")
-    print(inp)
+    client = userConnection();
     wordList = parser(inp)
     commandWord = wordList[0]
     if commandWord == '/join':
-        server_connect()
+        client.server_IP = wordList[1]
+        client.port = int(wordList[2])
+        client.server_connect()
     elif commandWord == '/leave':
-        server_disconnect()
+        client.server_disconnect()
     elif commandWord == '/register':
-        register_alias()
+        client.register_alias(wordList[1])
+        client.userName = wordList[1]
     elif commandWord == '/store':
-        send_file(wordList[1])
+        fileName = wordList[1]
+        client.send_file(fileName)
     elif commandWord == '/dir':
-        fetch_dir()
+        client.fetch_dir()
     elif commandWord == '/get':
-        fetch_file(wordList[1])
+        fileName = wordList[1]
+        client.fetch_file(fileName)
     elif commandWord == '/?':
-        print_help()
+        client.print_help()
 
     if wordList[0] == "/exit":
         print("See you on the flip side")
