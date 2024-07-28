d8f0c5b65973a8376f2b54f2a75d414e23eac8cc
sperrfeuer7 <ccccaramat@gmail.com>
2024-07-28 18:17:40 +0800

    feat: add server disconnect

--- client.py
+++ client.py
@@ -20,12 +20,13 @@ def register_alias():
 
 # Disconnects from the current server
 def server_disconnect():
-    pass
+    s.close()
+    s = socket.socket()
 
 # Connects with the server
 def server_connect(host, port):
     s.connect((host,port))
-    pass
+
 
 # Prints the commands and their functions
 def print_help():
